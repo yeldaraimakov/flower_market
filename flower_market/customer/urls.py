ec5ac3d275fefad2b73c80584874d685cb198fce
yeldaraimakov <yeldar.aimakov@gmail.com>
2019-05-27 14:16:18 +0600

    base html created

--- flower_market/customer/urls.py
+++ flower_market/customer/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='flower_market/home.html'), name='home'),
     path('order-call/', views.order_call, name='order-call'),
     path('ordering/', views.ordering, name='ordering'),
+    path('delivery/', TemplateView.as_view(template_name='flower_market/delivery.html'), name='delivery'),
     path('success-order/', TemplateView.as_view(template_name='flower_market/success_order.html'), name='success_order'),
     path('card/', TemplateView.as_view(template_name='flower_market/card.html'), name='card'),
 ]
